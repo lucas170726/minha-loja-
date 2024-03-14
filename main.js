6aa97eca5c238d55fb185ca5b78e743d32556f60
lucas170726 <105953139+lucas170726@users.noreply.github.com>
2024-03-14 20:42:56 -0300

    ss

--- main.js
+++ main.js
@@ -6,6 +6,6 @@ for (let i = 0; i < botoes.length; i++) {
         for (let j = 0; j < botoes.length; j++) {
             botoes[j].classlist.remove('ativo');
         }
-        botoes[i].classlist.add('ativo');
+        botoes[i].classList.add('ativo');
     }
 }
