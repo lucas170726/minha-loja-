fd341fce8d10b6d5b3a32e115f079b0af65b264c
lucas170726 <105953139+lucas170726@users.noreply.github.com>
2024-03-14 20:34:00 -0300

    dd

--- js main.js
+++ js main.js
@@ -1,13 +1,11 @@
 const botaes = document.querySelectorAll('.botao');
 
-for (let i = 0; i< botoes.length; i++) {
-    botoes[i].onclik = function() {
-        
-        for (let j = 0; j< botoes.length; j++)(
-            botoes[j].classlist .remover('ativo')
-        )
-        }
-        botoes[i].classlist .add('ativo')
-        )
+for (let i = 0; i < botoes.length; i++) {
+    botoes[i].onclik = function () {
 
-console.log(botaes);
+        for (let j = 0; j < botoes.length; j++) {
+            botoes[j].classlist.remover('ativo')
+        }
+        botoes[i].classlist.add('ativo')
+    }
+}
