6c61855033907625fff2ba2ff555a81b499435a8
lucas170726 <105953139+lucas170726@users.noreply.github.com>
2024-03-14 20:44:21 -0300

    ss

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ for (let i = 0; i < botoes.length; i++) {
     botoes[i].onclik = function () {
 
         for (let j = 0; j < botoes.length; j++) {
-            botoes[j].classlist.remove('ativo');
+            botoes[j].classList.remove('ativo');
         }
         botoes[i].classList.add('ativo');
     }
