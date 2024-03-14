ae67699a0dc27c9b40b2af0e44db96cd194718aa
lucas170726 <105953139+lucas170726@users.noreply.github.com>
2024-03-14 20:29:20 -0300

    dzda

--- js main.js
+++ js main.js
@@ -1,3 +1,13 @@
 const botaes = document.querySelectorAll('.botao');
 
+for (let i = 0; i< botoes.length; i++) {
+    botoes[i].onclik = function() {
+        
+        for (let j = 0; j< botoes.length; j++)(
+            botoes[j].classlist .remover('ativo')
+        )
+        }
+        botoes[i].classlist .add('ativo')
+        )
+
 console.log(botaes);
