08a1d1758a966bd5bd1fb1c16b77f0d33d6e3f5d
lucas170726 <105953139+lucas170726@users.noreply.github.com>
2024-04-11 20:50:15 -0300

    ee

--- main.js
+++ main.js
@@ -13,6 +13,6 @@ for (let i = 0; i < botoes.length; i++) {
     }
 }
 
-const contadores = document.querySelectorAll('.contador');
+const contadores = document.querySelectorAll('.contador')
 const tempoObjetivo1 = new Date('2024-10-05');
-contadores[0].textcontent = tempoObjetivo1;
+contadores[0].textContent = tempoObjetivo1;
