b2c71e3314d173efed27dd9c41b62fd4554e78e3
lucas170726 <105953139+lucas170726@users.noreply.github.com>
2024-04-11 21:08:05 -0300

    rtuf

--- main.js
+++ main.js
@@ -15,7 +15,15 @@ for (let i = 0; i < botoes.length; i++) {
 
 const contadores = document.querySelectorAll('.contador')
 const tempoObjetivo1 = new Date('2024-10-05T00:00:00');
-let tempoAtual = new Date();
 
+contadores[0].textContent = calculatempo(tempoObjetivo1); 
 
-contadores[0].textContent = tempoObjetivo1-tempoAtual; 
+function calculatempo(tempoObjetivo1) {
+    let tempoAtual = new Date();
+    let tempoFinal = tempoObjetivo1-tempoAtual;
+    let segundos = Math.floor(tempoFinal / 10000);
+    let minutos = Math.floor(segundos / 60);
+    let horas = Math.floor (minutos / 60);
+    let dias = Math.floor (horas / 24);
+    return dias;
+}
