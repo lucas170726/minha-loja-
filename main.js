b2465e0551d978c424be3ded4619ae92d47468de
lucas170726 <105953139+lucas170726@users.noreply.github.com>
2024-04-18 20:36:53 -0300

    outy67

--- main.js
+++ main.js
@@ -25,5 +25,9 @@ function calculatempo(tempoObjetivo1) {
     let minutos = Math.floor(segundos / 60);
     let horas = Math.floor (minutos / 60);
     let dias = Math.floor (horas / 24);
+
+    ssegundos %= 60;
+    minutos %=60;
+    horas %=24;
     return dias  + 'dias' + horas + 'horas' + minutos  + 'minutos' + segundos + 'segundos';
 }
