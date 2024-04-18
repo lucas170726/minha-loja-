5f7e539cf7db893ac457a47e4d9dd655dc28e60b
lucas170726 <105953139+lucas170726@users.noreply.github.com>
2024-04-18 20:38:13 -0300

    cvgd

--- main.js
+++ main.js
@@ -26,8 +26,8 @@ function calculatempo(tempoObjetivo1) {
     let horas = Math.floor (minutos / 60);
     let dias = Math.floor (horas / 24);
 
-    ssegundos %= 60;
-    minutos %=60;
-    horas %=24;
+    segundos %= 60;
+    minutos %= 60;
+    horas %= 24;
     return dias  + 'dias' + horas + 'horas' + minutos  + 'minutos' + segundos + 'segundos' ;
 }
