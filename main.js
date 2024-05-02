d8821d3b97e091b72976e8e0acc1760aa11f71d2
lucas170726 <105953139+lucas170726@users.noreply.github.com>
2024-05-02 20:23:36 -0300

    eee

--- main.js
+++ main.js
@@ -16,27 +16,27 @@ for (let i = 0; i < botoes.length; i++) {
 const contadores = document.querySelectorAll('.contador')
 const tempoObjetivo1 = new Date('2024-10-05T00:00:00');
 
-const tempoObjetivo1 =objetivo 1 17 ('2024-10-05T00:00:00' );
-const tempoObjetivo1 = objetivo 2 20('2024-10-05T00:00:00');
-const tempoObjetivo1 = objetivo 3 10('2024-10-05T00:00:00');
+const tempoObjetivo2 = new Date('2024-10-05T00:00:00');
+const tempoObjetivo3 = new Date('2024-10-05T00:00:00');
+const tempoObjetivo4 = new Date('2024-10-05T00:00:00');
 
-cont textos = [tempoObjetivo1, tempoObjetivo2, tempoObjetivo3, tempoObjetivo4 ] ;
+const tempos = [tempoObjetivo1, tempoObjetivo2, tempoObjetivo3, tempoObjetivo4];
 
-for (let i = 0; i < contadores . length; i++)
-    contadores [i] . textContent = calculatempo (tempos [i] );
+for (let i = 0; i < contadores.length; i++)
+    contadores[i].textContent = calculatempo(tempos[i]);
 
-contadores[0].textContent = calculatempo(tempoObjetivo1); 
+contadores[0].textContent = calculatempo(tempoObjetivo1);
 
 function calculatempo(tempoObjetivo1) {
     let tempoAtual = new Date();
-    let tempoFinal = tempoObjetivo1-tempoAtual;
+    let tempoFinal = tempoObjetivo1 - tempoAtual;
     let segundos = Math.floor(tempoFinal / 1000);
     let minutos = Math.floor(segundos / 60);
-    let horas = Math.floor (minutos / 60);
-    let dias = Math.floor (horas / 24);
+    let horas = Math.floor(minutos / 60);
+    let dias = Math.floor(horas / 24);
 
     segundos %= 60;
     minutos %= 60;
     horas %= 24;
-    return dias + 'dias' + horas + 'horas' + minutos  + 'minutos' + segundos + 'segundos' ;
+    return dias + 'dias' + horas + 'horas' + minutos + 'minutos' + segundos + 'segundos';
 }
