ad355668e901cacb2318c0b62424ea859ccfaa31
lucas170726 <105953139+lucas170726@users.noreply.github.com>
2024-05-02 19:58:52 -0300

    hhhh

--- main.js
+++ main.js
@@ -16,6 +16,12 @@ for (let i = 0; i < botoes.length; i++) {
 const contadores = document.querySelectorAll('.contador')
 const tempoObjetivo1 = new Date('2024-10-05T00:00:00');
 
+const tempoObjetivo1 =objetivo 1 17 ('2024-10-05T00:00:00' );
+
+const tempoObjetivo1 = objetivo 2 20('2024-10-05T00:00:00');
+
+const tempoObjetivo1 = objetivo 3 10('2024-10-05T00:00:00');
+
 contadores[0].textContent = calculatempo(tempoObjetivo1); 
 
 function calculatempo(tempoObjetivo1) {
